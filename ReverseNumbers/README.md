8087cf16487b9a2036b4e65c814ac41733f9edcf
belkerf <jbelkerf@gmail.com>
2023-09-04 16:47:48 +0100

    adding a c program

---
+++ ReverseNumbers/README.md
@@ -0,0 +1,1 @@
+"this is a program to reverse the elements of an array" 
---
+++ ReverseNumbers/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i = 8;
+    int arr[] = {34,56,54,32,67,89,90,32,21};
+    while (i >= 0)
+    {
+        printf("%d\t",arr[i]);
+        i--;
+    }
+    return 0;
+}
